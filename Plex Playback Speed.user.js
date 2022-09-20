dfbbb6fd23a385f0f3a4c966a5ffeac9ecb6be2c
ZigZagT <i@zhangtao.cool>
2022-09-19 20:48:18 -0700

    fix updateUrl

--- Plex Playback Speed.user.js
+++ Plex Playback Speed.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Plex Playback Speed
 // @version      1.2.0
-// @updateURL    https://gist.githubusercontent.com/ZigZagT/b992bda82b5f7a2c9d214110273d3f3c/raw/bfaa6288230d939a30538998afd06f3ec360e7be/Plex%2520Playback%2520Speed.user.js
+// @updateURL    https://gist.githubusercontent.com/ZigZagT/b992bda82b5f7a2c9d214110273d3f3c/raw/Plex%2520Playback%2520Speed.user.js
 // @description  Add playback speed controls to plex web player with keyboard shortcuts
 // @author       ZigZagT
 // @include      /^https?://[^/]*plex[^/]*
